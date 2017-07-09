34f721b63f243cb716f5ed02e652fe2e12a422d1
3Anology <kent1026+3a@gmail.com>
2017-07-10 03:09:50 +0800

    Create C_7697_mqtt.ino

---
+++ C_7697_mqtt.ino
@@ -0,0 +1,132 @@
+/*
+  Web client
+
+  This sketch connects to a website (http://download.labs.mediatek.com)
+  using LinkIt 7697
+
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the Wifi.begin() call accordingly.
+
+  Circuit:
+  * LinkIt 7697
+
+  created 13 July 2010
+  by dlf (Metodo2 srl)
+  modified 31 May 2012
+  by Tom Igoe
+  modified Jan 2017
+  by MediaTek Labs
+*/
+
+#include <LWiFi.h>
+#include <PubSubClient.h>
+
+char ssid[] = "SSID";
+char password[] = "Password";
+char mqtt_server[] = "MQTT Broker IP address";
+char sub_topic[] = "7697 subscribe topic";
+char pub_topic[] = "7697 publish topic";
+char client_Id[] = "7697client-01-";
+ 
+int status = WL_IDLE_STATUS;
+
+WiFiClient mtclient;     
+PubSubClient client(mtclient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup() {
+    //Initialize serial and wait for port to open:
+    Serial.begin(9600);
+    //while (!Serial) {
+         // wait for serial port to connect. Needed for native USB port only
+    //}
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+
+    Serial.print("Publish message ");
+    Serial.println(msg);
+    client.publish(pub_topic, "hello world");
+}
+}
+
+
+void printWifiStatus() {                     //print Wifi status
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
+}
+
+void setup_wifi() {                       //setup Wifi
+   // attempt to connect to Wifi network:
+   Serial.print("Attempting to connect to SSID: ");
+   Serial.println(ssid);
+   WiFi.begin(ssid, password);
+   while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+    }
+    randomSeed(micros());
+    Serial.println("Connected to wifi");
+    printWifiStatus();
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {   //MQTT sub
+  Serial.print("Input Message arrived [");
+  Serial.print(sub_topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {  //reconnect MQTT
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = client_Id;
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(pub_topic, client_Id);
+      // ... and resubscribe
+      client.subscribe(sub_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
